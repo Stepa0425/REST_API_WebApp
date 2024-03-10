d843bad718d98a94bf8565f40e7d9b384bf42e7b
Stepa0425 <karapetyanstepa@gmail.com>
2024-03-10 17:07:59 +0300

    add interface and change Service

--- src/main/java/edu/bsuir/MobileOperator/services/UserService.java
+++ src/main/java/edu/bsuir/MobileOperator/services/UserService.java
@@ -1,10 +1,14 @@
 package edu.bsuir.MobileOperator.services;
 
 import edu.bsuir.MobileOperator.entities.User;
+import edu.bsuir.MobileOperator.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+@RequiredArgsConstructor
+public class UserService implements UserRepository{
+    private final UserRepository userRepository;
 
     public User getUser(){
         return new User("Алексей", "Махтей", "mac@gmail.com", "Минск, Гурского 2", "password", "335471898", false, 50);
