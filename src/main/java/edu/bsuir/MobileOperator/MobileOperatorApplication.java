4f8420c6d6ca5a9c9a6d603efdd4f4c5c4fa711a
Stepa0425 <karapetyanstepa@gmail.com>
2024-03-10 14:32:42 +0300

    inintial

---
+++ src/main/java/edu/bsuir/MobileOperator/MobileOperatorApplication.java
@@ -0,0 +1,11 @@
+package edu.bsuir.MobileOperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileOperatorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MobileOperatorApplication.class, args);
+	}
+}
